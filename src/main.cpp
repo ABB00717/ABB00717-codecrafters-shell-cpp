b355adb8e918d911c80b7c6a7e1ad1bec96117c7
ABB00717 <eric94528@gmail.com>
2025-03-04 11:25:43 +0800

    Constant input command

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,20 @@
 #include <iostream>
+#include <string>
+
+bool inputCommand(std::string& input) {
+  std::cout << "$ ";
+  if (std::getline(std::cin, input))
+    return true;
+  
+  return false;
+}
 
 int main() {
   // Flush after every std::cout / std:cerr
   std::cout << std::unitbuf;
   std::cerr << std::unitbuf;
 
-  // Uncomment this block to pass the first stage
-  std::cout << "$ ";
-
   std::string input;
-  std::getline(std::cin, input);
-
-  std::cout << input << ": command not found\n";
+  while (inputCommand(input))
+    std::cout << input << ": command not found\n";
 }
