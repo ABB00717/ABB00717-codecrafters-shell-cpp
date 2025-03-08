e886457647cda6627db3f0ce722737de4f8a6e10
ABB00717 <eric94528@gmail.com>
2025-03-08 23:06:59 +0800

    single quotes

--- src/utils.cpp
+++ src/utils.cpp
@@ -1,5 +1,7 @@
 #include "../include/utils.h"
+#include <cctype>
 #include <cstdio>
+#include <string>
 
 bool inputCommand(std::string &input) {
   std::cout << "$ ";
@@ -13,24 +15,51 @@ std::vector<std::string> parseInputs(const std::string& input) {
   std::stringstream ss(input);
   std::string token;
   std::vector<std::string> tokens;
-  while (ss >> token)
-    tokens.push_back(token);
+
+  char c;
+  bool insideQuotes = false;
+  std::string currentToken;
+
+  while (ss.get(c)) {
+    if (c == '\'') {
+      insideQuotes = !insideQuotes;
+      if (!insideQuotes) {
+        tokens.push_back(currentToken);
+        currentToken.clear();
+      }
+    } else if (std::isspace(c) && !insideQuotes) {
+      if (!currentToken.empty()) {
+        tokens.push_back(currentToken);
+        currentToken.clear();
+      }
+    } else {
+      currentToken += c;
+    }
+  }
+
+  if (!currentToken.empty()) {
+    tokens.push_back(currentToken);
+  }
 
   return tokens;
 }
 
 std::string getPath(std::string command) {
-  std::string path_env = std::getenv("PATH");
-
-  std::stringstream ss(path_env);
-  std::string path;
-
-  while (std::getline(ss, path, ':')) {
-    std::filesystem::path abs_path = path;
-    abs_path /= command;
-    if (std::filesystem::exists(abs_path)) {
-      return abs_path.string();
+  try {
+    std::string path_env = std::getenv("PATH");
+    
+    std::stringstream ss(path_env);
+    std::string path;
+    
+    while (std::getline(ss, path, ':')) {
+      std::filesystem::path abs_path = path;
+      abs_path /= command;
+      if (std::filesystem::exists(abs_path)) {
+        return abs_path.string();
+      }
     }
+  } catch (...) {
+    return "";
   }
 
   return "";
